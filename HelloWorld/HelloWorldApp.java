984333a24cc233fac39f20c3f4b6df84ffdae0a9
Myodong <gyehd1230@naver.com>
2022-08-05 19:10:58 +0900

    생활코딩
    생활코딩 공부

---
+++ HelloWorld/HelloWorldApp.java
@@ -0,0 +1,11 @@
+public class HelloWorldApp{
+	public static void main(String[] args) {
+		System.out.println("Hello world!!");
+	}
+} 
+
+//public class HelloWorldApp//파일명과동일해야함 HelloWorldApp{
+//	public static void main(String[] args)//main인 작동함 {
+//		System.out.println("Hello world!!");
+//	}
+//} 
