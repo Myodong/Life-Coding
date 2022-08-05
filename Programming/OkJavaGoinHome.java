caa37b0f41dad36e84960c94ed639f68c860dc4d
Myodong <gyehd1230@naver.com>
2022-08-05 19:46:41 +0900

    생활코딩
    생활코딩 공부

---
+++ Programming/OkJavaGoinHome.java
@@ -0,0 +1,28 @@
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Security;
+import org.opentutorials.iot.Lighting;
+
+public class OkJavaGoinHome {
+
+
+	public static void main(String[] args) {
+		
+		String id = "Myodon APT 1994";
+
+		//Elevator call
+		Elevator myElevator = new Elevator(id);
+		myElevator.callForUp(1);
+		
+		//Security off
+		Security mysecSecurity = new Security(id);
+		mysecSecurity.off();
+		//Light on
+		Lighting hallLamp = new Lighting(id+" / hall Lamp");
+		hallLamp.on();
+		Lighting floorLamp = new Lighting(id+" / hall Lamp");
+		floorLamp.on();
+		
+		
+	}
+
+}
