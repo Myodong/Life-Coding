b1f02653412bcdcfbac6d9940f64b62263f062c1
Myodong <gyehd1230@naver.com>
2023-02-07 09:51:39 +0900

    Create ClassApp.java

---
+++ Programming/ClassApp.java
@@ -0,0 +1,18 @@
+
+public class ClassApp {
+	
+	public static void main(String[] args) {
+		
+		// 3.14
+		System.out.println(Math.PI);
+		
+		// 소수정 자르기
+		System.out.println(Math.floor(1.6));
+		
+		// 소수점 올리기
+		System.out.println(Math.ceil(1.6));
+		
+		
+	}
+
+}
