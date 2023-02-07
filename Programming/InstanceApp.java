53bc6b1e06c059f7fbba7610d973da6b5801408d
Myodong <gyehd1230@naver.com>
2023-02-07 10:08:12 +0900

    Update InstanceApp.java

--- Programming/InstanceApp.java
+++ Programming/InstanceApp.java
@@ -6,11 +6,20 @@ public class InstanceApp {
 	public static void main(String[] args) throws IOException {
 		
 		PrintWriter p1 = new PrintWriter("result1.txt");
+		// new 뒤부터가 Constructors 이다 PrintWriter("result1.txt");
+		// p1 인스터스 이며
+		// 어떤 값이 올수 있냐 라는 것을 규제하기 위해 PrintWriter라는 클래스 이름을 적는다
+		
 		p1.write("Hello 1");
 		p1.close();
 		
 		PrintWriter p2 = new PrintWriter("result2.txt");
 		p2.write("Hello 2");
 		p2.close();
+		
+		// Constructors 가 없으면 일회용이라고 생각해라!
+		// 어디서 확인 가능한가 ? https://docs.oracle.com/javase/7/docs/api/
+		// 자바 api 에서 확인가능
+		
 	}
 }
