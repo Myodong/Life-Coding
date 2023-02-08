e4fff85d819b321d212383f63faf758f8a78e115
Myodong <gyehd1230@naver.com>
2023-02-08 10:13:49 +0900

    Update AccountingApp.java

--- MyApp/src/AccountingApp.java
+++ MyApp/src/AccountingApp.java
@@ -3,9 +3,14 @@ public class AccountingApp {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Value of supply : "+10000.0);
-		System.out.println("VAT: "+ (10000.0*0.1));
-
+		System.out.println("Value of supply : "+12345.0);
+		System.out.println("VAT : "+ (12345.0*0.1));
+		System.out.println("Total : "+ (12345.0+12345.0*0.1));
+		System.out.println("Expense : "+ (12345.0*0.3));
+		System.out.println("Income : "+ (12345.0-12345.0*0.3));
+		System.out.println("Divident1 : "+ (12345.0-12345.0*0.3)*0.5);
+		System.out.println("Divident2 : "+ (12345.0-12345.0*0.3)*0.3);
+		System.out.println("Divident3 : "+ (12345.0-12345.0*0.3)*0.2);
 	}
 
 }
