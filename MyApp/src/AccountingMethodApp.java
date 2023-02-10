7d0cf73fa87f891d4c1bf513a307c2190ddc4a56
Myodong <gyehd1230@naver.com>
2023-02-10 14:06:13 +0900

    Update AccountingMethodApp.java

--- MyApp/src/AccountingMethodApp.java
+++ MyApp/src/AccountingMethodApp.java
@@ -10,22 +10,33 @@ public class AccountingMethodApp {
 		valueOfSupply =10000.0;
         vatRate = 0.1;
         expenseRate = 0.3;
-        double vat = getVAT();
-        double total = getTotal();
-        double expense = getExpense();
-        double income = valueOfSupply - expense;
-        double dividend1 = income * 0.5;
-        double dividend2 = income * 0.3;
-        double dividend3 = income * 0.2;
- 
-        System.out.println("Value of supply : " + valueOfSupply);
-        System.out.println("VAT : " + vat);
-        System.out.println("Total : " + total);
-        System.out.println("Expense : " + expense);
-        System.out.println("Income : " + income);
-        System.out.println("Dividend 1 : " + dividend1);
-        System.out.println("Dividend 2 : " + dividend2);
-        System.out.println("Dividend 3 : " + dividend3);
+        print();
+	}
+
+	private static void print() {
+		System.out.println("Value of supply : " + valueOfSupply);
+        System.out.println("VAT : " + getVAT());
+        System.out.println("Total : " + getTotal());
+        System.out.println("Expense : " + getExpense());
+        System.out.println("Income : " + getIncome());
+        System.out.println("Dividend 1 : " + getDiviend1());
+        System.out.println("Dividend 2 : " + getDiviend2());
+        System.out.println("Dividend 3 : " + getDiviend3());
+	}
+
+	private static double getDiviend1() {
+		return getIncome() * 0.5;
+	}
+	private static double getDiviend2() {
+		return getIncome() * 0.3;
+	}
+	private static double getDiviend3() {
+		return getIncome() * 0.2;
+	}
+
+	private static double getIncome() {
+		double income = valueOfSupply - getExpense();
+		return income;
 	}
 
 	private static double getExpense() {
